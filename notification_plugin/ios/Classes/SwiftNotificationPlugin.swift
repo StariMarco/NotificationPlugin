23b53497a541713ba60e71335b49b395953cfc3b
Marco <stari.marco11@gmail.com>
2022-10-16 21:17:23 +0200

    iOS

--- notification_plugin/ios/Classes/SwiftNotificationPlugin.swift
+++ notification_plugin/ios/Classes/SwiftNotificationPlugin.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 
+@available(iOS 10.0, *)
 public class SwiftNotificationPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "notification_plugin", binaryMessenger: registrar.messenger())
@@ -9,6 +10,39 @@ public class SwiftNotificationPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if(call.method == "showNotification"){
+      showNotification(argsMap: call.arguments as! NSDictionary)
+      result(true)
+    }else{
+      result(FlutterMethodNotImplemented)
+    }
+  }
+    
+  private func showNotification(argsMap: NSDictionary){
+    UNUserNotificationCenter.current().delegate = self
+    
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {success, error in
+      if(!success){
+        print(error?.localizedDescription ?? "Notifications were not authorized")
+      }
+    })
+    
+    let content = UNMutableNotificationContent()
+    content.title = argsMap.value(forKey: "title") as! String
+    content.body = argsMap.value(forKey: "description") as! String
+    content.sound = UNNotificationSound.default
+    
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.01, repeats: false)
+    let id = argsMap.value(forKey: "id") as! Int
+    let request = UNNotificationRequest(identifier: String(id), content: content, trigger: trigger)
+    
+    UNUserNotificationCenter.current().add(request)
+  }
+}
+
+@available(iOS 10.0, *)
+extension SwiftNotificationPlugin: UNUserNotificationCenterDelegate{
+  public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    completionHandler(.alert)
   }
 }
